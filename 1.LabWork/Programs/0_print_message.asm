5747a1c75d98098aca5637be81954d6b9a08cff6
jayshreejgit <114432156+jayshreejgit@users.noreply.github.com>
2023-05-16 12:56:43 +0530

    Update 0_print_message.asm

--- 1.LabWork/Programs/0_print_message.asm
+++ 1.LabWork/Programs/0_print_message.asm
@@ -1,20 +1,20 @@
-.model small
-.stack
+.model small                                                ;defines the memory model to be used for the ALP
+.stack                                                      ; initialize stack segment
 
-.data
-message   db "Hello world, I'm learning Assembly !!!", "$"
+.data                                                       ;data segment begins here
+message   db "Hello world, I'm learning Assembly !!!", "$"  ;String gets stored in message 
 
-.code
+.code                                                       ;code segment begins here
 
 main   proc
-   mov   ax,seg message
-   mov   ds,ax
+   mov   ax,seg message                                     ;returns segment address for message  or moving segment address of message to ax
+   mov   ds,ax                                              ;moving contents of ax into ds register or transfers segment address into ds register
 
-   mov   ah,09
-   lea   dx,message
-   int   21h
+   mov   ah,09                                              ; prints the result in the screen
+   lea   dx,message                                         ;load the offset address of message
+   int   21h                                                ;system interrupt or call the interrupt handler for DOS input dispatche
 
-   mov   ax,4c00h
-   int   21h
-main   endp
-end main
+   mov   ax,4c00h                                           ;to terminate the program
+   int   21h                                                
+main   endp 
+end main                                                    ;end of program
